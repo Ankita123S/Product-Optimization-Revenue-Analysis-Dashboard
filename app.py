6d5e77dac1e12abec29a8e3b94f0eeb0842b8cf1
Ankita123S <ankitasingh06101999@gmail.com>
2026-04-15 00:44:54 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Coffee Revenue Analysis Dashboard")
+
+st.write("This project analyzes product performance and revenue contribution.")
+
+st.write("Dashboard created using Power BI.")
+
+st.write("Key Insights:")
+st.write("- Top products generate most revenue")
+st.write("- Coffee category dominates")
+st.write("- Some products need optimization")
